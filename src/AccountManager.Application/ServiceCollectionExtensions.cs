fb48bab5959ae85f52a259099dbcb1dcdfbe1fd6
Gabriele Pividori <impavidor@gmail.com>
2026-04-27 14:48:51 -0400

    Close #15: DI wiring + controller refactor + commit architecture docs

--- src/AccountManager.Application/ServiceCollectionExtensions.cs
+++ src/AccountManager.Application/ServiceCollectionExtensions.cs
@@ -1,4 +1,8 @@
+using AccountManager.Application.Administration;
+using AccountManager.Application.SelfService;
+using AccountManager.Common.Errors;
 using AccountManager.Domain.Administration;
+using AccountManager.Domain.SelfService;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AccountManager.Application;
@@ -9,6 +13,21 @@ public static class ServiceCollectionExtensions
     {
         services.AddScoped<IActivateContactService, ActivateContactService>();
         services.AddScoped<IDeleteContactService, DeleteContactService>();
+
+        services.AddScoped<ICommandHandler<VerifyContactCommand>, VerifyContactHandler>();
+        services.AddScoped<ICommandHandler<DeleteContactCommand>, DeleteContactHandler>();
+        services.AddScoped<IQueryHandler<GetContactQuery, ContactDto>, GetContactHandler>();
+        services.AddScoped<IQueryHandler<ListContactsQuery, IReadOnlyList<ContactSummaryDto>>, ListContactsHandler>();
+
+        services.AddScoped<ICommandHandler<RegisterProviderCommand>, RegisterProviderHandler>();
+        services.AddScoped<ICommandHandler<RegisterProviderAdminCommand>, RegisterProviderAdminHandler>();
+        services.AddScoped<ICommandHandler<RegisterSystemAdminCommand>, RegisterSystemAdminHandler>();
+        services.AddScoped<ICommandHandler<ChangeProviderNpiCommand>, ChangeProviderNpiHandler>();
+        services.AddScoped<ICommandHandler<ChangeNameCommand>, ChangeNameHandler>();
+        services.AddScoped<IQueryHandler<GetAccountQuery, AccountDto>, GetAccountHandler>();
+
+        services.Decorate(typeof(ICommandHandler<>), typeof(UnitOfWorkCommandHandlerDecorator<>));
+
         return services;
     }
 }
--- src/AccountManager.WebAPI/Controllers/AdministrationController.cs
+++ src/AccountManager.WebAPI/Controllers/AdministrationController.cs
@@ -1,6 +1,6 @@
 using AccountManager.Application;
 using AccountManager.Application.Administration;
-using AccountManager.Domain.Administration;
+using AccountManager.Common.Errors;
 using CSharpFunctionalExtensions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,46 +10,45 @@ namespace AccountManager.WebAPI.Controllers;
 public class AdministrationController : ControllerBase
 {
     private readonly ResultMapper _mapper;
+    private readonly ICommandHandler<VerifyContactCommand> _verifyContact;
+    private readonly ICommandHandler<DeleteContactCommand> _deleteContact;
+    private readonly IQueryHandler<GetContactQuery, ContactDto> _getContact;
+    private readonly IQueryHandler<ListContactsQuery, IReadOnlyList<ContactSummaryDto>> _listContacts;
 
-    public AdministrationController(ResultMapper mapper) => _mapper = mapper;
+    public AdministrationController(
+        ResultMapper mapper,
+        ICommandHandler<VerifyContactCommand> verifyContact,
+        ICommandHandler<DeleteContactCommand> deleteContact,
+        IQueryHandler<GetContactQuery, ContactDto> getContact,
+        IQueryHandler<ListContactsQuery, IReadOnlyList<ContactSummaryDto>> listContacts)
+    {
+        _mapper = mapper;
+        _verifyContact = verifyContact;
+        _deleteContact = deleteContact;
+        _getContact = getContact;
+        _listContacts = listContacts;
+    }
 
     [HttpPost("/administration/contacts/{id}/verify")]
-    public async Task<IActionResult> VerifyContact(
-        Guid id,
-        [FromServices] IActivateContactService service,
-        [FromServices] ICurrentActor actor)
-    {
-        var command = VerifyContactCommand.Create(id).Value;
-        var result = await new VerifyContactHandler(service, actor).Handle(command);
-        return result.Match(_mapper.Map, _mapper.MapError);
-    }
+    public async Task<IActionResult> VerifyContact(Guid id) =>
+        await VerifyContactCommand.Create(id)
+            .Bind(_verifyContact.Handle)
+            .Match(_mapper.Map, _mapper.MapError);
 
     [HttpDelete("/administration/contacts/{id}")]
-    public async Task<IActionResult> DeleteContact(
-        Guid id,
-        [FromServices] IDeleteContactService service,
-        [FromServices] ICurrentActor actor)
-    {
-        var command = DeleteContactCommand.Create(id).Value;
-        var result = await new DeleteContactHandler(service, actor).Handle(command);
-        return result.Match(_mapper.Map, _mapper.MapError);
-    }
+    public async Task<IActionResult> DeleteContact(Guid id) =>
+        await DeleteContactCommand.Create(id)
+            .Bind(_deleteContact.Handle)
+            .Match(_mapper.Map, _mapper.MapError);
 
     [HttpGet("/administration/contacts/{id}")]
-    public async Task<IActionResult> GetContact(
-        Guid id,
-        [FromServices] IContactProjector projector)
-    {
-        var query = GetContactQuery.Create(id).Value;
-        var result = await new GetContactHandler(projector).Handle(query);
-        return result.Match(dto => (IActionResult)Ok(dto), _mapper.MapError);
-    }
+    public async Task<IActionResult> GetContact(Guid id) =>
+        await GetContactQuery.Create(id)
+            .Bind(_getContact.Handle)
+            .Match(dto => (IActionResult)Ok(dto), _mapper.MapError);
 
     [HttpGet("/administration/contacts")]
-    public async Task<IActionResult> ListContacts(
-        [FromServices] IContactProjector projector)
-    {
-        var result = await new ListContactsHandler(projector).Handle(new ListContactsQuery());
-        return result.Match(list => (IActionResult)Ok(list), _mapper.MapError);
-    }
+    public async Task<IActionResult> ListContacts() =>
+        await _listContacts.Handle(new ListContactsQuery())
+            .Match(list => (IActionResult)Ok(list), _mapper.MapError);
 }
